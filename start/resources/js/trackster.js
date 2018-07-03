52b80dbe363fb0fd52ad58e1981560740aac3b69
mac <applewhi@gmail.com>
2018-07-03 13:58:03 -0500

    site working but failing one test

--- start/resources/js/trackster.js
+++ start/resources/js/trackster.js
@@ -3,7 +3,7 @@ const API_KEY = '2fb57a71fac6ec22ae942500940fcb53';
 $(document).ready(function() {
   $('#search-btn').click(function() {
     Trackster.searchTracksByTitle($('#search-box').val());
-    $('#track-list').empty();
+
   });
 });
 
@@ -12,19 +12,21 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
-  tracks.forEach(function(track){
+  $('#track-list').empty();
 
+  tracks.forEach(function(track){
     var mediumAlbumArt = track.image[1]["#text"];
     var mockTrack =
         '<div class="row track">'+
-          '<a class="col-xs-2 col-xs-offset-1" href="' + track.URL + '><i class="fa fa-play-circle-o"></i></a>' +
+          '<div class="col-xs-2 play-button">' +
+            '<a href="' + track.url + '"target="_blank"><i class="fa fa-play-circle-o"></i></a>' +
+            '</div>' +
           '<div class="col-xs-2">' + track.name + '</div>' +
           '<div class="col-xs-2">' + track.artist + '</div>' +
           '<img class="col-xs-2" src=' + mediumAlbumArt + '>' +
           '<div class="col-xs-2">' + track.listeners + '</div>' +
         '</div>';
     $('#track-list').append(mockTrack);
-    console.log(track);
   });
 };
 
@@ -35,7 +37,6 @@ Trackster.renderTracks = function(tracks) {
 Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
-    datatype: 'jsonp',
     success: function(tracks){
       Trackster.renderTracks(tracks.results.trackmatches.track);
     }
